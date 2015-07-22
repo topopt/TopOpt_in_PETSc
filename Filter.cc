f60c03958385c65ef71ed351044d48e008a9e653
Erik Andreassen <erand@mek.dtu.dk>
2015-07-22 12:08:48 +0200

    Should now work with 64-bit integers

--- Filter.cc
+++ Filter.cc
@@ -25,29 +25,29 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 
 Filter::Filter(TopOpt *opt){
-  // Set all pointers to NULL
-  H=NULL;  
-  Hs=NULL;
-  da_elem=NULL;
-  pdef=NULL;
-  
-  // Call the setup method
-  SetUp(opt);
+	// Set all pointers to NULL
+	H=NULL;  
+	Hs=NULL;
+	da_elem=NULL;
+	pdef=NULL;
+
+	// Call the setup method
+	SetUp(opt);
 }
 
 Filter::~Filter(){
-    // Deallocate data
-    if (Hs!=NULL){ VecDestroy(&Hs); }
-    if (H!=NULL){ MatDestroy(&H); }
-    if (da_elem!=NULL){ DMDestroy(&da_elem); }
-    if (pdef!=NULL){delete pdef; }
-    
+	// Deallocate data
+	if (Hs!=NULL){ VecDestroy(&Hs); }
+	if (H!=NULL){ MatDestroy(&H); }
+	if (da_elem!=NULL){ DMDestroy(&da_elem); }
+	if (pdef!=NULL){delete pdef; }
+
 }
 
 // Filter design variables
 PetscErrorCode Filter::FilterProject(TopOpt *opt){
 	PetscErrorCode ierr;
-  	
+
 	// Filter the design variables or copy to xPhys
 	// STANDARD FILTER
 	if (opt->filter == 1){
@@ -59,212 +59,212 @@ PetscErrorCode Filter::FilterProject(TopOpt *opt){
 	else if (opt->filter == 2 ){
 		ierr = pdef->FilterProject(opt->x, opt->xPhys); CHKERRQ(ierr);
 		// Check for bound violation: simple, but cheap check!
-                PetscScalar *xp;
-                PetscInt locsiz;
-                VecGetArray(opt->xPhys,&xp);
-                VecGetLocalSize(opt->xPhys,&locsiz);
-                for (PetscInt i=0;i<locsiz;i++){
-                        if (xp[i] < 0.0){
-                                if (PetscAbsReal(xp[i]) > 1.0e-4){
-                                        PetscPrintf(PETSC_COMM_WORLD,"BOUND VIOLATION IN PDEFILTER - INCREASE RMIN OR MESH RESOLUTION: xPhys = %f\n",xp[i]);
-                                }
-                                xp[i]= 0.0;
-                        }
-                        if (xp[i] > 1.0){
-                                if (PetscAbsReal(xp[i]-1.0) > 1.0e-4){
-                                        PetscPrintf(PETSC_COMM_WORLD,"BOUND VIOLATION IN PDEFILTER - INCREASE RMIN OR MESH RESOLUTION: xPhys = %f\n",xp[i]);
-                                }
-                                xp[i]=1.0;
-                        }
+		PetscScalar *xp;
+		PetscInt locsiz;
+		VecGetArray(opt->xPhys,&xp);
+		VecGetLocalSize(opt->xPhys,&locsiz);
+		for (PetscInt i=0;i<locsiz;i++){
+			if (xp[i] < 0.0){
+				if (PetscAbsReal(xp[i]) > 1.0e-4){
+					PetscPrintf(PETSC_COMM_WORLD,"BOUND VIOLATION IN PDEFILTER - INCREASE RMIN OR MESH RESOLUTION: xPhys = %f\n",xp[i]);
+				}
+				xp[i]= 0.0;
+			}
+			if (xp[i] > 1.0){
+				if (PetscAbsReal(xp[i]-1.0) > 1.0e-4){
+					PetscPrintf(PETSC_COMM_WORLD,"BOUND VIOLATION IN PDEFILTER - INCREASE RMIN OR MESH RESOLUTION: xPhys = %f\n",xp[i]);
+				}
+				xp[i]=1.0;
+			}
 
-                }
-                VecRestoreArray(opt->xPhys,&xp);
+		}
+		VecRestoreArray(opt->xPhys,&xp);
 	}
 	// COPY IN CASE OF SENSITIVITY FILTER
 	else {	ierr = VecCopy(opt->x,opt->xPhys); CHKERRQ(ierr); }
-		
+
 	return ierr;
 }
-    
+
 // Filter the sensitivities
 PetscErrorCode Filter::Gradients(TopOpt *opt){
-	
+
 	PetscErrorCode ierr;
-        // Chainrule/Filter for the sensitivities
+	// Chainrule/Filter for the sensitivities
 	if (opt->filter == 0)
-	// Filter the sensitivities, df,dg
-        {
-	        Vec xtmp;
-                ierr = VecDuplicate(opt->x,&xtmp);  CHKERRQ(ierr);
-	        VecPointwiseMult(xtmp,opt->dfdx,opt->x);
-                MatMult(H,xtmp,opt->dfdx);
-	        VecPointwiseDivide(xtmp,opt->dfdx,Hs);
-	        VecPointwiseDivide(opt->dfdx,xtmp,opt->x);
-                VecDestroy(&xtmp);
-        }
-        else if (opt->filter == 1) {
-		// Filter the densities, df,dg: STANDARD FILTER
-	        Vec xtmp;
-                ierr = VecDuplicate(opt->x,&xtmp);  CHKERRQ(ierr);
-		// dfdx
-                VecPointwiseDivide(xtmp,opt->dfdx,Hs);
+		// Filter the sensitivities, df,dg
+	{
+		Vec xtmp;
+		ierr = VecDuplicate(opt->x,&xtmp);  CHKERRQ(ierr);
+		VecPointwiseMult(xtmp,opt->dfdx,opt->x);
 		MatMult(H,xtmp,opt->dfdx);
-	        // dgdx
+		VecPointwiseDivide(xtmp,opt->dfdx,Hs);
+		VecPointwiseDivide(opt->dfdx,xtmp,opt->x);
+		VecDestroy(&xtmp);
+	}
+	else if (opt->filter == 1) {
+		// Filter the densities, df,dg: STANDARD FILTER
+		Vec xtmp;
+		ierr = VecDuplicate(opt->x,&xtmp);  CHKERRQ(ierr);
+		// dfdx
+		VecPointwiseDivide(xtmp,opt->dfdx,Hs);
+		MatMult(H,xtmp,opt->dfdx);
+		// dgdx
 		VecPointwiseDivide(xtmp,opt->dgdx[0],Hs);
 		MatMult(H,xtmp,opt->dgdx[0]);
 		// tidy up
-                VecDestroy(&xtmp);
+		VecDestroy(&xtmp);
 	}
 	else if (opt->filter == 2){
 		// Filter the densities, df,dg: PDE FILTER  
 		ierr = pdef->Gradients(opt->dfdx,opt->dfdx); CHKERRQ(ierr);
 		ierr = pdef->Gradients(opt->dgdx[0],opt->dgdx[0]); CHKERRQ(ierr);
 	}
-  
+
 	return ierr;
 }
 
 
 PetscErrorCode Filter::SetUp(TopOpt *opt){
-  
+
 	PetscErrorCode ierr;
-	
-  	// Get rank from MPI
+
+	// Get rank from MPI
 	PetscMPIInt myrank;
 	MPI_Comm_rank(PETSC_COMM_WORLD, &myrank);
-  
+
 	if (opt->filter==0 || opt->filter==1){
-	
-// Extract information from the nodal mesh
-	PetscInt M,N,P,md,nd,pd; 
-	DMBoundaryType bx, by, bz;
-	DMDAStencilType stype;
-	ierr = DMDAGetInfo(opt->da_nodes,NULL,&M,&N,&P,&md,&nd,&pd,NULL,NULL,&bx,&by,&bz,&stype); CHKERRQ(ierr);
-	
-// Find the element size
-	Vec lcoor;
-	DMGetCoordinatesLocal(opt->da_nodes,&lcoor);
-	PetscScalar *lcoorp;
-	VecGetArray(lcoor,&lcoorp);
-	
-	PetscInt nel, nen;
-	const PetscInt *necon;
-	DMDAGetElements_3D(opt->da_nodes,&nel,&nen,&necon);
-	
-	PetscScalar dx,dy,dz;
-	// Use the first element to compute the dx, dy, dz
-	dx = lcoorp[3*necon[0*nen + 1]+0]-lcoorp[3*necon[0*nen + 0]+0];
-	dy = lcoorp[3*necon[0*nen + 2]+1]-lcoorp[3*necon[0*nen + 1]+1];
-	dz = lcoorp[3*necon[0*nen + 4]+2]-lcoorp[3*necon[0*nen + 0]+2];
-	VecRestoreArray(lcoor,&lcoorp);
-	
-// Create the minimum element connectivity shit
-	PetscInt ElemConn;
-	// Check dx,dy,dz and find max conn for a given rmin
-	ElemConn = (PetscInt)PetscMax(ceil(opt->rmin/dx)-1,PetscMax(ceil(opt->rmin/dy)-1,ceil(opt->rmin/dz)-1));
-	ElemConn = PetscMin(ElemConn,PetscMin((M-1)/2,PetscMin((N-1)/2,(P-1)/2)));
-	
-	// The following is needed due to roundoff errors 
-	PetscInt tmp;
-        MPI_Allreduce(&ElemConn, &tmp, 1,MPIU_INT, MPI_MAX,PETSC_COMM_WORLD );
-        ElemConn = tmp;
-	
-	// Print to screen: mesh overlap!
-	PetscPrintf(PETSC_COMM_WORLD,"# Filter radius rmin = %f results in a stencil of %i elements \n",opt->rmin,ElemConn);
-	
-// Find the geometric partitioning of the nodal mesh, so the element mesh will coincide 
-	PetscInt *Lx=new PetscInt[md];
-	PetscInt *Ly=new PetscInt[nd];
-	PetscInt *Lz=new PetscInt[pd];
-	
-	// get number of nodes for each partition
-	const PetscInt *LxCorrect, *LyCorrect, *LzCorrect;
-	DMDAGetOwnershipRanges(opt->da_nodes, &LxCorrect, &LyCorrect, &LzCorrect); 
 
-	// subtract one from the lower left corner.
-	for (int i=0; i<md; i++){
-		Lx[i] = LxCorrect[i];
-		if (i==0){Lx[i] = Lx[i]-1;}
-	}
-	for (int i=0; i<nd; i++){
-		Ly[i] = LyCorrect[i];
-		if (i==0){Ly[i] = Ly[i]-1;}
-	}
-	for (int i=0; i<pd; i++){
-		Lz[i] = LzCorrect[i];
-		if (i==0){Lz[i] = Lz[i]-1;}
-	}
+		// Extract information from the nodal mesh
+		PetscInt M,N,P,md,nd,pd; 
+		DMBoundaryType bx, by, bz;
+		DMDAStencilType stype;
+		ierr = DMDAGetInfo(opt->da_nodes,NULL,&M,&N,&P,&md,&nd,&pd,NULL,NULL,&bx,&by,&bz,&stype); CHKERRQ(ierr);
 
-	// Create the element grid:
-	DMDACreate3d(PETSC_COMM_WORLD,bx,by,bz,stype,M-1,N-1,P-1,md,nd,pd,
-			1,ElemConn,Lx,Ly,Lz,&da_elem);
-	
-	
-	// Set the coordinates: from 0+dx/2 to xmax-dx/2 and so on
-	PetscScalar xmax = (M-1)*dx;
-	PetscScalar ymax = (N-1)*dy;
-	PetscScalar zmax = (P-1)*dz;
-	DMDASetUniformCoordinates(da_elem , dx/2.0,xmax-dx/2.0, dy/2.0,ymax-dy/2.0, dz/2.0,zmax-dz/2.0);
+		// Find the element size
+		Vec lcoor;
+		DMGetCoordinatesLocal(opt->da_nodes,&lcoor);
+		PetscScalar *lcoorp;
+		VecGetArray(lcoor,&lcoorp);
 
-	// Allocate and assemble
-	DMCreateMatrix(da_elem,&H);
-	DMCreateGlobalVector(da_elem,&Hs);
-	
-	//Set the filter matrix and vector
-	// 1. assemble H
-	DMGetCoordinatesLocal(da_elem,&lcoor);
-	VecGetArray(lcoor,&lcoorp);
-	DMDALocalInfo info;
-	DMDAGetLocalInfo(da_elem,&info);
-	// Outer loop is local part = find row
-	for (PetscInt k=info.zs; k<info.zs+info.zm; k++) {
-		for (PetscInt j=info.ys; j<info.ys+info.ym; j++) {
-			for (PetscInt i=info.xs; i<info.xs+info.xm; i++) {
-				PetscInt col = (i-info.gxs) + (j-info.gys)*(info.gxm) + (k-info.gzs  )*(info.gxm)*(info.gym);
-				// Loop over nodes (including ghosts) within a cubic domain with center at (i,j,k)
-				for (PetscInt k2=std::max(k-info.sw,0);k2<=std::min(k+info.sw,info.mz);k2++){
-					for (PetscInt j2=std::max(j-info.sw,0);j2<=std::min(j+info.sw,info.my);j2++){
-						for (PetscInt i2=std::max(i-info.sw,0);i2<=std::min(i+info.sw,info.mx);i2++){
-							PetscInt row = (i2-info.gxs) + (j2-info.gys)*(info.gxm) + (k2-info.gzs  )*(info.gxm)*(info.gym);
-							PetscScalar dist = 0.0;
-							for(PetscInt kk=0; kk<3; kk++){
-								dist = dist + PetscPowScalar(lcoorp[3*col+kk]-lcoorp[3*row+kk],2.0);
-							}
-							dist = PetscSqrtScalar(dist);
-							if (dist<opt->rmin){
-								dist = opt->rmin-dist;
-								MatSetValuesLocal(H, 1, &col, 1, &row, &dist, INSERT_VALUES); 
+		PetscInt nel, nen;
+		const PetscInt *necon;
+		DMDAGetElements_3D(opt->da_nodes,&nel,&nen,&necon);
+
+		PetscScalar dx,dy,dz;
+		// Use the first element to compute the dx, dy, dz
+		dx = lcoorp[3*necon[0*nen + 1]+0]-lcoorp[3*necon[0*nen + 0]+0];
+		dy = lcoorp[3*necon[0*nen + 2]+1]-lcoorp[3*necon[0*nen + 1]+1];
+		dz = lcoorp[3*necon[0*nen + 4]+2]-lcoorp[3*necon[0*nen + 0]+2];
+		VecRestoreArray(lcoor,&lcoorp);
+
+		// Create the minimum element connectivity shit
+		PetscInt ElemConn;
+		// Check dx,dy,dz and find max conn for a given rmin
+		ElemConn = (PetscInt)PetscMax(ceil(opt->rmin/dx)-1,PetscMax(ceil(opt->rmin/dy)-1,ceil(opt->rmin/dz)-1));
+		ElemConn = PetscMin(ElemConn,PetscMin((M-1)/2,PetscMin((N-1)/2,(P-1)/2)));
+
+		// The following is needed due to roundoff errors 
+		PetscInt tmp;
+		MPI_Allreduce(&ElemConn, &tmp, 1,MPIU_INT, MPI_MAX,PETSC_COMM_WORLD );
+		ElemConn = tmp;
+
+		// Print to screen: mesh overlap!
+		PetscPrintf(PETSC_COMM_WORLD,"# Filter radius rmin = %f results in a stencil of %i elements \n",opt->rmin,ElemConn);
+
+		// Find the geometric partitioning of the nodal mesh, so the element mesh will coincide 
+		PetscInt *Lx=new PetscInt[md];
+		PetscInt *Ly=new PetscInt[nd];
+		PetscInt *Lz=new PetscInt[pd];
+
+		// get number of nodes for each partition
+		const PetscInt *LxCorrect, *LyCorrect, *LzCorrect;
+		DMDAGetOwnershipRanges(opt->da_nodes, &LxCorrect, &LyCorrect, &LzCorrect); 
+
+		// subtract one from the lower left corner.
+		for (int i=0; i<md; i++){
+			Lx[i] = LxCorrect[i];
+			if (i==0){Lx[i] = Lx[i]-1;}
+		}
+		for (int i=0; i<nd; i++){
+			Ly[i] = LyCorrect[i];
+			if (i==0){Ly[i] = Ly[i]-1;}
+		}
+		for (int i=0; i<pd; i++){
+			Lz[i] = LzCorrect[i];
+			if (i==0){Lz[i] = Lz[i]-1;}
+		}
+
+		// Create the element grid:
+		DMDACreate3d(PETSC_COMM_WORLD,bx,by,bz,stype,M-1,N-1,P-1,md,nd,pd,
+				1,ElemConn,Lx,Ly,Lz,&da_elem);
+
+
+		// Set the coordinates: from 0+dx/2 to xmax-dx/2 and so on
+		PetscScalar xmax = (M-1)*dx;
+		PetscScalar ymax = (N-1)*dy;
+		PetscScalar zmax = (P-1)*dz;
+		DMDASetUniformCoordinates(da_elem , dx/2.0,xmax-dx/2.0, dy/2.0,ymax-dy/2.0, dz/2.0,zmax-dz/2.0);
+
+		// Allocate and assemble
+		DMCreateMatrix(da_elem,&H);
+		DMCreateGlobalVector(da_elem,&Hs);
+
+		//Set the filter matrix and vector
+		// 1. assemble H
+		DMGetCoordinatesLocal(da_elem,&lcoor);
+		VecGetArray(lcoor,&lcoorp);
+		DMDALocalInfo info;
+		DMDAGetLocalInfo(da_elem,&info);
+		// Outer loop is local part = find row
+		for (PetscInt k=info.zs; k<info.zs+info.zm; k++) {
+			for (PetscInt j=info.ys; j<info.ys+info.ym; j++) {
+				for (PetscInt i=info.xs; i<info.xs+info.xm; i++) {
+					PetscInt col = (i-info.gxs) + (j-info.gys)*(info.gxm) + (k-info.gzs  )*(info.gxm)*(info.gym);
+					// Loop over nodes (including ghosts) within a cubic domain with center at (i,j,k)
+					for (PetscInt k2=PetscMax(k-info.sw,0);k2<=PetscMin(k+info.sw,info.mz);k2++){
+						for (PetscInt j2=PetscMax(j-info.sw,0);j2<=PetscMin(j+info.sw,info.my);j2++){
+							for (PetscInt i2=PetscMax(i-info.sw,0);i2<=PetscMin(i+info.sw,info.mx);i2++){
+								PetscInt row = (i2-info.gxs) + (j2-info.gys)*(info.gxm) + (k2-info.gzs  )*(info.gxm)*(info.gym);
+								PetscScalar dist = 0.0;
+								for(PetscInt kk=0; kk<3; kk++){
+									dist = dist + PetscPowScalar(lcoorp[3*col+kk]-lcoorp[3*row+kk],2.0);
+								}
+								dist = PetscSqrtScalar(dist);
+								if (dist<opt->rmin){
+									dist = opt->rmin-dist;
+									MatSetValuesLocal(H, 1, &col, 1, &row, &dist, INSERT_VALUES); 
+								}
 							}
 						}
 					}
 				}
 			}
 		}
-	}
-	// Assemble H:
-	MatAssemblyBegin(H, MAT_FINAL_ASSEMBLY);
-	MatAssemblyEnd(H, MAT_FINAL_ASSEMBLY);
-	// 2. compute the Hs, i.e. sum the rows
-	Vec dummy;
-	VecDuplicate(Hs,&dummy);
-	VecSet(dummy,1.0);
-	MatMult(H,dummy,Hs);
+		// Assemble H:
+		MatAssemblyBegin(H, MAT_FINAL_ASSEMBLY);
+		MatAssemblyEnd(H, MAT_FINAL_ASSEMBLY);
+		// 2. compute the Hs, i.e. sum the rows
+		Vec dummy;
+		VecDuplicate(Hs,&dummy);
+		VecSet(dummy,1.0);
+		MatMult(H,dummy,Hs);
 
-	// Clean up
-	VecRestoreArray(lcoor,&lcoorp);
-	VecDestroy(&dummy);
-	delete [] Lx;
-	delete [] Ly;
-	delete [] Lz;
-	
+		// Clean up
+		VecRestoreArray(lcoor,&lcoorp);
+		VecDestroy(&dummy);
+		delete [] Lx;
+		delete [] Ly;
+		delete [] Lz;
+
 	} 
 	else if (opt->filter==2){
 		// ALLOCATE AND SETUP THE PDE FILTER CLASS
 		pdef = new PDEFilt(opt);
 	}
-	  
+
 	return ierr;
-  
+
 }
 
 
