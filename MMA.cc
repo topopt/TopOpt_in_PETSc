66474142f28de4d80f653bbeaf90a95599a0ab5b
topopt <topoptapp@gmail.com>
2020-11-09 11:45:40 +0100

    Update MMA.cc

--- MMA.cc
+++ MMA.cc
@@ -3,6 +3,22 @@
 #include <iostream>
 #include <math.h>
 
+/* -----------------------------------------------------------------------------
+Authors: Niels Aage
+ Copyright (C) 2013-2019,
+This MMA implementation is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This Module is distributed in the hope that it will be useful,implementation
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public
+License along with this Module; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+-------------------------------------------------------------------------- */
+
 MMA::MMA(PetscInt nn, PetscInt mm, PetscInt kk, Vec xo1t, Vec xo2t, Vec Ut, Vec Lt, PetscScalar* at, PetscScalar* ct,
          PetscScalar* dt) {
     n = nn;
